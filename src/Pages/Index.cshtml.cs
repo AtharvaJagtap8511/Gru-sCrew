29a93dd4fffd41c2737888c771492662ba243f8b
Sheetalkan99 <skandhare@seattleu.edu>
2023-09-29 15:01:36 -0700

    Added name Sheetal Kandhare to IndexModel Comment in Index.cshtml.cs

--- src/Pages/Index.cshtml.cs
+++ src/Pages/Index.cshtml.cs
@@ -9,7 +9,7 @@ using ContosoCrafts.WebSite.Services;
 namespace ContosoCrafts.WebSite.Pages
 {
     /// <summary>
-    /// Mike Koenig
+    /// Sheetal Kandhare
     /// </summary>
     public class IndexModel : PageModel
     {
