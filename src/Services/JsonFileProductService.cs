fe1ca3de18ee5e79ae2166a9a2b10bb99e92a1e2
Sheetalkan99 <skandhare@seattleu.edu>
2023-10-21 16:11:11 -0700

    Added UpdateData() function in JsonFileProductService.cs

--- src/Services/JsonFileProductService.cs
+++ src/Services/JsonFileProductService.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text.Json;
 
 using ContosoCrafts.WebSite.Models;
@@ -9,7 +10,7 @@ using Microsoft.AspNetCore.Hosting;
 
 namespace ContosoCrafts.WebSite.Services
 {
-   public class JsonFileProductService
+    public class JsonFileProductService
     {
         public JsonFileProductService(IWebHostEnvironment webHostEnvironment)
         {
@@ -25,7 +26,18 @@ namespace ContosoCrafts.WebSite.Services
 
         public IEnumerable<ProductModel> GetProducts()
         {
-            using(var jsonFileReader = File.OpenText(JsonFileName))
+            using (var jsonFileReader = File.OpenText(JsonFileName))
+            {
+                return JsonSerializer.Deserialize<ProductModel[]>(jsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+            }
+        }
+        public IEnumerable<ProductModel> GetAllData()
+        {
+            using (var jsonFileReader = File.OpenText(JsonFileName))
             {
                 return JsonSerializer.Deserialize<ProductModel[]>(jsonFileReader.ReadToEnd(),
                     new JsonSerializerOptions
@@ -35,11 +47,43 @@ namespace ContosoCrafts.WebSite.Services
             }
         }
 
+        public ProductModel UpdateData(ProductModel data)
+
+        {
+
+            var products = GetAllData();
+
+            var productData = products.FirstOrDefault(x => x.Id.Equals(data.Id));
+
+            if (productData == null)
+
+            {
+
+                return null;
+
+            }
+
+            // Update the data to the new passed in values
+
+            productData.Title = data.Title;
+
+            productData.Description = data.Description.Trim();
+
+            productData.Url = data.Url;
+
+            productData.Image = data.Image;
+
+            /*SaveData(products);*/
+
+            return productData;
+
+        }
+
         public void AddRating(string productId, int rating)
         {
             var products = GetProducts();
 
-            if(products.First(x => x.Id == productId).Ratings == null)
+            if (products.First(x => x.Id == productId).Ratings == null)
             {
                 products.First(x => x.Id == productId).Ratings = new int[] { rating };
             }
@@ -50,14 +94,15 @@ namespace ContosoCrafts.WebSite.Services
                 products.First(x => x.Id == productId).Ratings = ratings.ToArray();
             }
 
-            using(var outputStream = File.OpenWrite(JsonFileName))
+
+            using (var outputStream = File.OpenWrite(JsonFileName))
             {
                 JsonSerializer.Serialize<IEnumerable<ProductModel>>(
                     new Utf8JsonWriter(outputStream, new JsonWriterOptions
                     {
                         SkipValidation = true,
                         Indented = true
-                    }), 
+                    }),
                     products
                 );
             }
