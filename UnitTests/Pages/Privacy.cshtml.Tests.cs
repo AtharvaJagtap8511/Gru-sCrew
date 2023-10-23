877c5631533cfbeaec99f52ac12f294452c332a8
Sheetalkan99 <skandhare@seattleu.edu>
2023-10-22 23:06:45 -0700

    Added Testcase for Privacy.cshtml in Privacy.cshtml.Tests.cs

--- UnitTests/Pages/Privacy.cshtml.Tests.cs
+++ UnitTests/Pages/Privacy.cshtml.Tests.cs
@@ -1,97 +1,103 @@
-using System.Diagnostics;
-
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Routing;
-using Microsoft.AspNetCore.Mvc.ModelBinding;
-using Microsoft.AspNetCore.Mvc.ViewFeatures;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Routing;
-using Microsoft.AspNetCore.Hosting;
+using ContosoCrafts.WebSite.Pages;
 using Microsoft.Extensions.Logging;
-
+using Moq;
 using NUnit.Framework;
 
-using Moq;
 
-using ContosoCrafts.WebSite.Pages;
-using ContosoCrafts.WebSite.Services;
+ 
 
 namespace UnitTests.Pages.Privacy
+
 {
+
+    /// <summary>
+
+    /// Class that has unit test cases related to Privacy page
+
+    /// </summary>
+
     public class PrivacyTests
+
     {
+
+        // creating an instance
+
         #region TestSetup
-        public static IUrlHelperFactory urlHelperFactory;
-        public static DefaultHttpContext httpContextDefault;
-        public static IWebHostEnvironment webHostEnvironment;
-        public static ModelStateDictionary modelState;
-        public static ActionContext actionContext;
-        public static EmptyModelMetadataProvider modelMetadataProvider;
-        public static ViewDataDictionary viewData;
-        public static TempDataDictionary tempData;
-        public static PageContext pageContext;
 
         public static PrivacyModel pageModel;
 
+
+
+        /// <summary>
+
+        /// Initializing test
+
+        /// </summary>
+
         [SetUp]
+
         public void TestInitialize()
+
         {
-            httpContextDefault = new DefaultHttpContext()
-            {
-                TraceIdentifier = "trace",
-                //RequestServices = serviceProviderMock.Object,
-            };
-            httpContextDefault.HttpContext.TraceIdentifier = "trace";
-
-            modelState = new ModelStateDictionary();
-
-            actionContext = new ActionContext(httpContextDefault, httpContextDefault.GetRouteData(), new PageActionDescriptor(), modelState);
-
-            modelMetadataProvider = new EmptyModelMetadataProvider();
-            viewData = new ViewDataDictionary(modelMetadataProvider, modelState);
-            tempData = new TempDataDictionary(httpContextDefault, Mock.Of<ITempDataProvider>());
-
-            pageContext = new PageContext(actionContext)
-            {
-                ViewData = viewData,
-                HttpContext = httpContextDefault
-            };
-
-            var mockWebHostEnvironment = new Mock<IWebHostEnvironment>();
-            mockWebHostEnvironment.Setup(m => m.EnvironmentName).Returns("Hosting:UnitTestEnvironment");
-            mockWebHostEnvironment.Setup(m => m.WebRootPath).Returns("../../../../src/bin/Debug/net5.0/wwwroot");
-            mockWebHostEnvironment.Setup(m => m.ContentRootPath).Returns("./data/");
 
             var MockLoggerDirect = Mock.Of<ILogger<PrivacyModel>>();
-            JsonFileProductService productService;
 
-            productService = new JsonFileProductService(mockWebHostEnvironment.Object);
+
 
             pageModel = new PrivacyModel(MockLoggerDirect)
+
             {
-                PageContext = pageContext,
-                TempData = tempData,
+
+                PageContext = TestHelper.PageContext,
+
+                TempData = TestHelper.TempData,
+
             };
+
         }
+
+
 
         #endregion TestSetup
 
+
+
+        /// <summary>
+
+        /// Test case to validate that on valid activity a valid RequestId is returned
+
+        /// </summary>
+
         #region OnGet
+
         [Test]
+
         public void OnGet_Valid_Activity_Set_Should_Return_RequestId()
+
         {
+
             // Arrange
 
+
+
             // Act
+
             pageModel.OnGet();
+
+
 
             // Reset
 
+
+
             // Assert
+
             Assert.AreEqual(true, pageModel.ModelState.IsValid);
+
         }
 
         #endregion OnGet
+
     }
+
 }
