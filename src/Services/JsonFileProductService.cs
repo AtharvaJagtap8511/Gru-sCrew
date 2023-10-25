0aeb5e66de2679cb4683759b11a241e7a22c9414
apatil2297 <apatil2@seattleu.edu>
2023-10-24 22:28:26 -0700

    Added Save Functionality on Save button in JsonFileProductService.cs

--- src/Services/JsonFileProductService.cs
+++ src/Services/JsonFileProductService.cs
@@ -1,7 +1,12 @@
+
 using System.Collections.Generic;
+
 using System.IO;
+
 using System.Linq;
+
 using System.Runtime.InteropServices;
+
 using System.Text.Json;
 
 using ContosoCrafts.WebSite.Models;
@@ -9,42 +14,73 @@ using ContosoCrafts.WebSite.Models;
 using Microsoft.AspNetCore.Hosting;
 
 namespace ContosoCrafts.WebSite.Services
+
 {
+
     public class JsonFileProductService
+
     {
+
         public JsonFileProductService(IWebHostEnvironment webHostEnvironment)
+
         {
+
             WebHostEnvironment = webHostEnvironment;
+
         }
 
         public IWebHostEnvironment WebHostEnvironment { get; }
 
         private string JsonFileName
+
         {
+
             get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "products.json"); }
+
         }
 
         public IEnumerable<ProductModel> GetProducts()
+
         {
+
             using (var jsonFileReader = File.OpenText(JsonFileName))
+
             {
+
                 return JsonSerializer.Deserialize<ProductModel[]>(jsonFileReader.ReadToEnd(),
+
                     new JsonSerializerOptions
+
                     {
+
                         PropertyNameCaseInsensitive = true
+
                     });
+
             }
+
         }
+
         public IEnumerable<ProductModel> GetAllData()
+
         {
+
             using (var jsonFileReader = File.OpenText(JsonFileName))
+
             {
+
                 return JsonSerializer.Deserialize<ProductModel[]>(jsonFileReader.ReadToEnd(),
+
                     new JsonSerializerOptions
+
                     {
+
                         PropertyNameCaseInsensitive = true
+
                     });
+
             }
+
         }
 
         public ProductModel UpdateData(ProductModel data)
@@ -73,39 +109,97 @@ namespace ContosoCrafts.WebSite.Services
 
             productData.Image = data.Image;
 
-            /*SaveData(products);*/
+            SaveData(products);
 
             return productData;
 
         }
 
-        public void AddRating(string productId, int rating)
+        /// <summary>
+
+        /// Save All products data to storage
+
+        /// </summary>
+
+        private void SaveData(IEnumerable<ProductModel> products)
+
         {
+
+            using (var outputStream = File.Create(JsonFileName))
+
+            {
+
+                JsonSerializer.Serialize<IEnumerable<ProductModel>>(
+
+                    new Utf8JsonWriter(outputStream, new JsonWriterOptions
+
+                    {
+
+                        SkipValidation = true,
+
+                        Indented = true
+
+                    }),
+
+                    products
+
+                );
+
+            }
+
+        }
+
+        public void AddRating(string productId, int rating)
+
+        {
+
             var products = GetProducts();
 
             if (products.First(x => x.Id == productId).Ratings == null)
+
             {
+
                 products.First(x => x.Id == productId).Ratings = new int[] { rating };
+
             }
+
             else
+
             {
+
                 var ratings = products.First(x => x.Id == productId).Ratings.ToList();
+
                 ratings.Add(rating);
+
                 products.First(x => x.Id == productId).Ratings = ratings.ToArray();
+
             }
 
 
             using (var outputStream = File.OpenWrite(JsonFileName))
+
             {
+
                 JsonSerializer.Serialize<IEnumerable<ProductModel>>(
+
                     new Utf8JsonWriter(outputStream, new JsonWriterOptions
+
                     {
+
                         SkipValidation = true,
+
                         Indented = true
+
                     }),
+
                     products
+
                 );
+
             }
+
         }
+
     }
+
 }
