ac3c7d39356f67c8ad6b8b8adad27c078afcab21
Chandrashekhar Pawar <cpawar@seattleu.edu>
2023-10-26 22:20:49 -0700

    Added Age field in product module.

--- src/Models/ProductModel.cs
+++ src/Models/ProductModel.cs
@@ -8,18 +8,17 @@ namespace ContosoCrafts.WebSite.Models
     {
         public string Id { get; set; }
         public string Maker { get; set; }
-        
-        public string Email { get; set; }
+
         [JsonPropertyName("img")]
         public string Image { get; set; }
+        public int Age { get; set; }
         public string Url { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public int[] Ratings { get; set; }
 
-
         public override string ToString() => JsonSerializer.Serialize<ProductModel>(this);
 
- 
+
     }
 }
