49afd5b8ec3b806fbf9872c8e80f16346eb517a3
Chandrashekhar Pawar <cpawar@seattleu.edu>
2023-10-26 22:16:50 -0700

    Added Age field in JsonFileProductservice.cs

--- src/Services/JsonFileProductService.cs
+++ src/Services/JsonFileProductService.cs
@@ -1,225 +1,109 @@
-
 using System.Collections.Generic;
-
 using System.IO;
-
 using System.Linq;
-
 using System.Runtime.InteropServices;
-
 using System.Text.Json;
-
 using ContosoCrafts.WebSite.Models;
-
 using Microsoft.AspNetCore.Hosting;
 
 namespace ContosoCrafts.WebSite.Services
-
 {
-
     public class JsonFileProductService
-
     {
-
         public JsonFileProductService(IWebHostEnvironment webHostEnvironment)
-
         {
-
             WebHostEnvironment = webHostEnvironment;
-
         }
 
         public IWebHostEnvironment WebHostEnvironment { get; }
 
         private string JsonFileName
-
         {
-
             get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "products.json"); }
-
         }
 
         public IEnumerable<ProductModel> GetProducts()
-
         {
-
             using (var jsonFileReader = File.OpenText(JsonFileName))
-
             {
-
                 return JsonSerializer.Deserialize<ProductModel[]>(jsonFileReader.ReadToEnd(),
-
                     new JsonSerializerOptions
-
                     {
-
                         PropertyNameCaseInsensitive = true
-
                     });
-
             }
-
         }
 
         public IEnumerable<ProductModel> GetAllData()
-
         {
-
             using (var jsonFileReader = File.OpenText(JsonFileName))
-
             {
-
                 return JsonSerializer.Deserialize<ProductModel[]>(jsonFileReader.ReadToEnd(),
-
                     new JsonSerializerOptions
-
                     {
-
                         PropertyNameCaseInsensitive = true
-
                     });
-
             }
-
         }
 
         public ProductModel UpdateData(ProductModel data)
-
         {
-
             var products = GetAllData();
-
             var productData = products.FirstOrDefault(x => x.Id.Equals(data.Id));
-
             if (productData == null)
-
             {
-
                 return null;
-
             }
-
-            // Update the data to the new passed in values
-
+            // Update the data to the new passed-in values
             productData.Title = data.Title;
-
             productData.Age = data.Age;
-
             productData.Description = data.Description.Trim();
-            productData.Email = data.Email;
             productData.Url = data.Url;
-
             productData.Image = data.Image;
-            
-
             SaveData(products);
-
             return productData;
-
         }
-
-        /// <summary>
-        /// DeleteData is a REST call to delete data
-        /// </summary>
-        /// <param name="id"></param>
-        /// <returns></returns>
-        public ProductModel DeleteData(string id)
-        {
-            // Get the current set, and append the new record to it
-            var dataSet = GetAllData();
-            var data = dataSet.FirstOrDefault(m => m.Id.Equals(id));
-
-            var newDataSet = GetAllData().Where(m => m.Id.Equals(id) == false);
-
-            SaveData(newDataSet);
-
-            return data;
-        }
-        /// <summary>
-
-        /// Save All products data to storage
-
-        /// </summary>
 
         private void SaveData(IEnumerable<ProductModel> products)
-
         {
-
             using (var outputStream = File.Create(JsonFileName))
-
             {
-
                 JsonSerializer.Serialize<IEnumerable<ProductModel>>(
-
                     new Utf8JsonWriter(outputStream, new JsonWriterOptions
-
                     {
-
                         SkipValidation = true,
-
                         Indented = true
-
                     }),
-
                     products
-
                 );
-
             }
-
         }
 
         public void AddRating(string productId, int rating)
-
         {
-
             var products = GetProducts();
-
             if (products.First(x => x.Id == productId).Ratings == null)
-
             {
-
                 products.First(x => x.Id == productId).Ratings = new int[] { rating };
-
             }
-
             else
-
             {
-
                 var ratings = products.First(x => x.Id == productId).Ratings.ToList();
-
                 ratings.Add(rating);
-
                 products.First(x => x.Id == productId).Ratings = ratings.ToArray();
-
             }
-
 
             using (var outputStream = File.OpenWrite(JsonFileName))
-
             {
-
                 JsonSerializer.Serialize<IEnumerable<ProductModel>>(
-
                     new Utf8JsonWriter(outputStream, new JsonWriterOptions
-
                     {
-
                         SkipValidation = true,
-
                         Indented = true
-
                     }),
-
                     products
-
                 );
-
             }
-
         }
-
     }
-
 }
