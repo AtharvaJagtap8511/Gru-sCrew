44030b4be28707ecedf9529fdb2574a7fcd45c5c
Atharva Jagtap <atharvajagtap8511@gmail.com>
2023-10-30 19:28:59 -0700

    Added test cases for Create.cshtml.cs in Create.cshtml.Tests.cs

--- src/Pages/Product/Create.cshtml.cs
+++ src/Pages/Product/Create.cshtml.cs
@@ -1,16 +1,3 @@
-//using Microsoft.AspNetCore.Mvc;
-//using Microsoft.AspNetCore.Mvc.RazorPages;
-
-//namespace ContosoCrafts.WebSite.Pages.Product
-//{
-//    public class CreateModel : PageModel
-//    {
-//        public void OnGet()
-//        {
-//        }
-//    }
-//}
-
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using ContosoCrafts.WebSite.Models;
