97735fee786548c04df20ed8835da048fbb1cbaf
Atharva Jagtap <atharvajagtap8511@gmail.com>
2023-11-05 12:13:33 -0800

    Updated comments for ProductModel.cs in Models folder

--- src/Models/ProductModel.cs
+++ src/Models/ProductModel.cs
@@ -4,22 +4,44 @@ using System.Text.Json.Serialization;
 
 namespace ContosoCrafts.WebSite.Models
 {
+    /// <summary>
+    /// Model class for Product
+    /// </summary>
     public class ProductModel
     {
+        // get or set the Product ID
         public string Id { get; set; }
+
+        // get or set the Maker of the product
         public string Maker { get; set; }
 
+        //get or set the image URL to JSON
         [JsonPropertyName("img")]
         public string Image { get; set; }
+
+        //get or set the Age of the Profile
         public int Age { get; set; }
+
+        //get or set the image URL to JSON
         public string Url { get; set; }
+
+        //get or set the Title of the Profile
         public string Title { get; set; }
+
+        //get or set the Skills of the Profile
         public string Skills { get; set; }
+
+        //get or set the Description of the Profile
         public string Description { get; set; }
+
+        //get or set the Email of the Profile
         public string Email { get; set; }
+
+        //get or set the Ratings of the Product
         public int[] Ratings { get; set; }
         public string ErrorMessage { get; set; }
 
+        //JsonSerializer to serialize ProductModel
         public override string ToString() => JsonSerializer.Serialize<ProductModel>(this);
 
 
