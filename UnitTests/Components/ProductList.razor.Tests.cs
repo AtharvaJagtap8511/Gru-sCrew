3f0fedc2a1d484896c10b0f804b9964e1257a54a
apatil2297 <apatil2@seattleu.edu>
2023-11-14 11:28:20 -0800

    Added file ProductList.razor.Tests.cs

---
+++ UnitTests/Components/ProductList.razor.Tests.cs
@@ -0,0 +1,21 @@
+﻿using ContosoCrafts.WebSite.Components;
+using ContosoCrafts.WebSite.Models;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using static ContosoCrafts.WebSite.Controllers.ProductsController;
+
+namespace UnitTests.Components
+{
+    public class ProductRazorTests
+    {
+        /// <summary>
+        /// You will need to configure this handler in the Web.config file of your 
+        /// web and register it with IIS before being able to use it. For more information
+        /// see the following link: https://go.microsoft.com/?linkid=8101007
+        /// </summary>
+        
+        
+    }
+}
