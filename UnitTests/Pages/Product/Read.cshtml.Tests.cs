e2bf20037f8b518a0765f45d6ab9b5e4bcb0aa76
apatil2297 <apatil2@seattleu.edu>
2023-11-13 16:03:48 -0800

    Modifies Testcases in Read.cshtml.Tests.cs

--- UnitTests/Pages/Product/Read.cshtml.Tests.cs
+++ UnitTests/Pages/Product/Read.cshtml.Tests.cs
@@ -1,52 +1,128 @@
-using System.Linq;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using NUnit.Framework;
-using ContosoCrafts.WebSite.Pages.Product;
 
-namespace UnitTests.Pages.Product.Index
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+
+using Moq;
+
+using NUnit.Framework;
+
+using ContosoCrafts.WebSite.Pages.Product;
+using ContosoCrafts.WebSite.Services;
+
+namespace UnitTests.Pages.Product.Read
 {
-    /// <summary>
-    /// Class containing unit test cases for the Index page
-    /// </summary>
     public class ReadTests
     {
-        // Creating an instance
         #region TestSetup
+        public static IUrlHelperFactory urlHelperFactory;
+        public static DefaultHttpContext httpContextDefault;
+        public static IWebHostEnvironment webHostEnvironment;
+        public static ModelStateDictionary modelState;
+        public static ActionContext actionContext;
+        public static EmptyModelMetadataProvider modelMetadataProvider;
+        public static ViewDataDictionary viewData;
+        public static TempDataDictionary tempData;
         public static PageContext pageContext;
 
-        /// <summary>
-        /// Creating an instance
-        /// </summary>
-        public static IndexModel pageModel;
+        public static ReadModel pageModel;
 
-        /// <summary>
-        /// Test initialization before each test case is executed
-        /// </summary>
         [SetUp]
         public void TestInitialize()
         {
-            // Code for initialization
-            pageModel = new IndexModel(TestHelper.ProductService)
+            httpContextDefault = new DefaultHttpContext()
+            {
+                //RequestServices = serviceProviderMock.Object,
+            };
+
+            modelState = new ModelStateDictionary();
+
+            actionContext = new ActionContext(httpContextDefault, httpContextDefault.GetRouteData(), new PageActionDescriptor(), modelState);
+
+            modelMetadataProvider = new EmptyModelMetadataProvider();
+            viewData = new ViewDataDictionary(modelMetadataProvider, modelState);
+            tempData = new TempDataDictionary(httpContextDefault, Mock.Of<ITempDataProvider>());
+
+            pageContext = new PageContext(actionContext)
+            {
+                ViewData = viewData,
+            };
+
+            var mockWebHostEnvironment = new Mock<IWebHostEnvironment>();
+            mockWebHostEnvironment.Setup(m => m.EnvironmentName).Returns("Hosting:UnitTestEnvironment");
+            mockWebHostEnvironment.Setup(m => m.WebRootPath).Returns("../../../../src/bin/Debug/net7.0/wwwroot");
+            mockWebHostEnvironment.Setup(m => m.ContentRootPath).Returns("./data/");
+
+            var MockLoggerDirect = Mock.Of<ILogger<IndexModel>>();
+            JsonFileProductService productService;
+
+            productService = new JsonFileProductService(mockWebHostEnvironment.Object);
+
+            pageModel = new ReadModel(productService)
             {
             };
         }
+
         #endregion TestSetup
 
-        /// <summary>
-        /// On making a GET call, the request should return all the products
-        /// </summary>
         #region OnGet
+
+        // OnGet on Read should test functionality of a Read of a specific product
+        // Tests should include making sure that Read returns the Product in its payload with a valid ID
+        // Tests should include InvalidID's
+
+        // OnPost is not handled.
+
+        /// <summary>
+        /// Return a Product with a given Id.
+        /// We will create a Record, and then search for it
+        /// 
+        /// </summary>
         [Test]
+
         public void OnGet_Valid_Should_Return_Products()
+        {
+            // Arrange 
+
+            // Act
+            pageModel.OnGet("jenlooper-cactus");
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual("James Johnson", pageModel.Product.Title);
+
+            // Reset
+
+            return;
+        }
+
+        [Test]
+        public void OnGet_InValid_Should_Return_InvalidState()
         {
             // Arrange
 
             // Act
-            pageModel.OnGet();
+            pageModel.OnGet("jenlooper-cactus2"); // Does not exist
+
+            
 
             // Assert
-            Assert.AreEqual(true, pageModel.ModelState.IsValid);
-            Assert.AreEqual(true, pageModel.Products.ToList().Any());
+            // Store whether the ModelState is valid for later assert
+            var stateIsValid = pageModel.ModelState.IsValid;
+            Assert.AreEqual(true, stateIsValid);
+
+            // Reset
+            // This should remove the error we added
+            pageModel.ModelState.Clear();
+
+
+            return;
         }
         #endregion OnGet
     }
